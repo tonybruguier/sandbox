e1d8edfd56d89120a597ec259cdbc435dd2b2641
Tony Bruguier <tony.bruguier@gmail.com>
2021-01-24 21:50:39 +0000

    Using MPS

--- mps_simulation.py
+++ mps_simulation.py
@@ -7,7 +7,7 @@ circuit = circuit_n12_m14_s0_e0_pEFGH.CIRCUIT
 qubit_order = circuit_n12_m14_s0_e0_pEFGH.QUBIT_ORDER
 initial_state = 0
 
-# Dense simulation
+# # Dense simulation
 # dense_simulator = cirq.Simulator()
 # dense_final = dense_simulator.simulate(circuit, qubit_order=qubit_order, initial_state=initial_state)
 # final_state_vector = dense_final.final_state_vector
@@ -16,5 +16,3 @@ initial_state = 0
 mps_simulator = ccq.mps_simulator.MPSSimulator()
 mps_final = mps_simulator.simulate(circuit, qubit_order=qubit_order, initial_state=initial_state)
 final_state_vector = mps_finalactual.final_state.to_numpy()
-
-breakpoint()
