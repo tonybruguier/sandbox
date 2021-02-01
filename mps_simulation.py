e0b82ce91ead93514abe9759295a7b6707425d9d
Tony Bruguier <tony.bruguier@gmail.com>
2021-02-01 15:50:49 +0000

    Generic circuit + grouping

--- mps_simulation.py
+++ mps_simulation.py
@@ -5,32 +5,31 @@ import resource
 import sys
 import traceback
 
-soft, hard = resource.getrlimit(resource.RLIMIT_AS)
-#resource.setrlimit(resource.RLIMIT_AS, (1 * 2 ** 30, soft))
+_, hard = resource.getrlimit(resource.RLIMIT_AS)
 resource.setrlimit(resource.RLIMIT_AS, ((1024 + 512) * 2 ** 20, hard))
-
 
 try:
     # Load the circuit
-    from n12_m14 import circuit_n12_m14_s0_e0_pEFGH
-    circuit = circuit_n12_m14_s0_e0_pEFGH.CIRCUIT
-    qubit_order = circuit_n12_m14_s0_e0_pEFGH.QUBIT_ORDER
+    from n40_m14 import circuit_n12_m14_s0_e0_pEFGH as example
+    circuit = example.CIRCUIT
+    qubit_order = example.QUBIT_ORDER
     initial_state = 0
 
     # MPS simulation
-    grouping = {qubit: qubit.col for qubit in qubit_order}
+    min_diag = min([qubit.col - qubit.row for qubit in qubit_order])
+    grouping = {qubit: (qubit.col - qubit.row - min_diag) for qubit in qubit_order}
 
-    mps_simulator = ccq.mps_simulator.MPSSimulator(rsum2_cutoff=1e-2, grouping=grouping)
+    mps_simulator = ccq.mps_simulator.MPSSimulator(rsum2_cutoff=0.1, grouping=grouping)
     mps_final = mps_simulator.simulate(circuit, qubit_order=qubit_order, initial_state=initial_state)
-    mps_final_state_vector = mps_final.final_state.to_numpy()
 
     print('%s' % (mps_final.final_state.estimation_stats()))
 
     # Dense simulation
+    mps_final_state_vector = mps_final.final_state.to_numpy()
+
     dense_simulator = cirq.Simulator()
     dense_final = dense_simulator.simulate(circuit, qubit_order=qubit_order, initial_state=initial_state)
     dense_final_state_vector = dense_final.final_state_vector
-
 
     fidelity = np.einsum('i,j,j,i->',
         dense_final_state_vector.conj(),
