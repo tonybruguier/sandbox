c3ea1ef1bf25724eb150d46fcfd540ef39786c8f
Tony Bruguier <tony.bruguier@gmail.com>
2021-02-01 04:56:21 +0000

    Grouping, exact fidelity

--- mps_simulation.py
+++ mps_simulation.py
@@ -1,15 +1,14 @@
 import cirq
 import cirq.contrib.quimb as ccq
+import numpy as np
 import resource
 import sys
 import traceback
 
 soft, hard = resource.getrlimit(resource.RLIMIT_AS)
 #resource.setrlimit(resource.RLIMIT_AS, (1 * 2 ** 30, soft))
-resource.setrlimit(resource.RLIMIT_AS, (1 * 2 ** 30, hard))
+resource.setrlimit(resource.RLIMIT_AS, ((1024 + 512) * 2 ** 20, hard))
 
-# sys.stdout = open('stdout.txt', 'w')
-# sys.stderr = open('stderr.txt', 'w')
 
 try:
     # Load the circuit
@@ -18,15 +17,28 @@ try:
     qubit_order = circuit_n12_m14_s0_e0_pEFGH.QUBIT_ORDER
     initial_state = 0
 
-    # # Dense simulation
-    # dense_simulator = cirq.Simulator()
-    # dense_final = dense_simulator.simulate(circuit, qubit_order=qubit_order, initial_state=initial_state)
-    # final_state_vector = dense_final.final_state_vector
+    # Dense simulation
+    dense_simulator = cirq.Simulator()
+    dense_final = dense_simulator.simulate(circuit, qubit_order=qubit_order, initial_state=initial_state)
+    dense_final_state_vector = dense_final.final_state_vector
 
     # MPS simulation
-    mps_simulator = ccq.mps_simulator.MPSSimulator(rsum2_cutoff=1e-9)
+    grouping = {qubit: qubit.col for qubit in qubit_order}
+
+    mps_simulator = ccq.mps_simulator.MPSSimulator(rsum2_cutoff=1e-2, grouping=grouping)
     mps_final = mps_simulator.simulate(circuit, qubit_order=qubit_order, initial_state=initial_state)
-    final_state_vector = mps_final.final_state.to_numpy()
+    mps_final_state_vector = mps_final.final_state.to_numpy()
+
+    fidelity = np.einsum('i,j,j,i->',
+        dense_final_state_vector.conj(),
+        dense_final_state_vector,
+        mps_final_state_vector.conj(),
+        mps_final_state_vector)
+
+    print('%s' % (mps_final.final_state.estimation_stats()))
+
+    print('fidelity=%.3e' % (fidelity.real))
+
 except MemoryError as error:
     print('TONYBOOM caught memory error: %s' % (error))
     traceback.print_exc()
--- set_pythonpath.sh
+++ set_pythonpath.sh
@@ -1,3 +1,3 @@
 # You should run it with 'source set_pythonpath.sh'
 
-PYTHONPATH="/home/ubuntu/Dropbox/AWS_EC2_shared/git/mps:/home/ubuntu/Dropbox/AWS_EC2_shared/git/martinis_et_al_data"
+PYTHONPATH="/home/ubuntu/Dropbox/AWS_EC2_shared/git/mps_grp:/home/ubuntu/Dropbox/AWS_EC2_shared/git/martinis_et_al_data"
