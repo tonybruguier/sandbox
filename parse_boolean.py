ae62011b6575fb5bfb20c0e9720a6337171fe73c
Tony Bruguier <tony.bruguier@gmail.com>
2021-04-03 06:01:26 +0000

    boolean

--- parse_boolean.py
+++ parse_boolean.py
@@ -1,11 +1,21 @@
 import math
 
+import numpy as np
 from sympy.parsing.sympy_parser import parse_expr
 from sympy.logic.boolalg import And, Not, Or, Xor
 from sympy.core.symbol import Symbol
 from typing import Dict, Tuple
 
+import cirq
+
+# References:
+# [1] On the representation of Boolean and real functions as Hamil tonians for quantum computing
+#     by StuartHadfield
+# [2] https://www.youtube.com/watch?v=AOKM9BkweVU is a useful intro
+# [3] https://github.com/rsln-s/IEEE_QW_2020/blob/master/Slides.pdf
+
 class HamiltonianList():
+    """A container class of Boolean function as equation (2) or [1]"""
     def __init__(self, hamiltonians: Dict[Tuple[int, ...], float]):
         self.hamiltonians = {h: w for h, w in hamiltonians.items() if math.fabs(w) > 1e-12}
 
@@ -55,17 +65,21 @@ class HamiltonianList():
         return HamiltonianList({(i, ): 1.0})
 
 
-def build_hamiltonian_from_boolean(boolean_expr, name_to_id  = None) -> HamiltonianList:
-    """Builds the Hamiltonian representation of Boolean expression as per:
-    On the representation of Boolean and real functions as Hamil tonians for quantum computing
-    StuartHadfield
+def build_hamiltonian_from_boolean(boolean_expr, name_to_id) -> HamiltonianList:
+    """Builds the Hamiltonian representation of Boolean expression as per [1]:
+
+    Args:
+        boolean_expr: A Sympy expression containing symbols and Boolean operations
+        name_to_id: A dictionary from symbol name to an integer, typically built by calling
+            get_name_to_id().
+
+    Return:
+        The HamiltonianList that represents the Boolean expression.
     """
-    if not name_to_id:
-        name_to_id = {symbol.name: i for i, symbol in enumerate(boolean_expr.free_symbols)}
 
     if isinstance(boolean_expr, (And, Not, Or, Xor)):
         sub_hamiltonians = [build_hamiltonian_from_boolean(sub_boolean_expr, name_to_id) for sub_boolean_expr in boolean_expr.args]
-        # We apply the equalities of theorem 1
+        # We apply the equalities of theorem 1 of [1].
         if isinstance(boolean_expr, And):
             hamiltonian = HamiltonianList.I()
             for sub_hamiltonian in sub_hamiltonians:
@@ -83,14 +97,44 @@ def build_hamiltonian_from_boolean(boolean_expr, name_to_id  = None) -> Hamilton
                 hamiltonian = hamiltonian + sub_hamiltonian - 2.0 * hamiltonian * sub_hamiltonian
         return hamiltonian
     elif isinstance(boolean_expr, Symbol):
-        # Table 1, entry for 'x' is '1/2.I - 1/2.Z'
+        # Table 1 of [1], entry for 'x' is '1/2.I - 1/2.Z'
         i = name_to_id[boolean_expr.name]
         return 0.5 * HamiltonianList.I() - 0.5 * HamiltonianList.Z(i)
     else:
         raise ValueError('Unsupported type: %s' % (type(boolean_expr)))
 
+def get_name_to_id(boolean_expr):
+    return {symbol.name: i for i, symbol in enumerate(boolean_expr.free_symbols)}
 
-boolean = parse_expr('~q0')
-hamiltonian = build_hamiltonian_from_boolean(boolean)
+def build_circuit_from_hamiltonian(hamiltonian, name_to_id, theta):
+    qubits = [cirq.NamedQubit(name) for name in name_to_id.keys()]
+    circuit = cirq.Circuit()
+
+    circuit.append(cirq.H.on_each(*qubits))
+
+    for h, w in hamiltonian.hamiltonians.items():
+        for i in range(1, len(h)):
+            circuit.append(cirq.CNOT(qubits[h[i]], qubits[h[0]]))
+
+        if len(h) >= 1:
+            circuit.append(cirq.Rz(rads=(theta * w)).on(qubits[h[0]]))
+
+        for i in range(1, len(h)):
+            circuit.append(cirq.CNOT(qubits[h[i]], qubits[h[0]]))
+
+    return circuit, qubits
+
+boolean = parse_expr('q0 & ~q1 & q2')
+name_to_id = get_name_to_id(boolean)
+hamiltonian = build_hamiltonian_from_boolean(boolean, name_to_id)
 
 print(hamiltonian)
+
+theta = 0.1 * math.pi
+circuit, qubits = build_circuit_from_hamiltonian(hamiltonian, name_to_id, theta)
+
+print(circuit)
+
+phi = cirq.Simulator().simulate(circuit, qubit_order=qubits, initial_state=0).state_vector()
+alpha = np.arctan2(phi.real, phi.imag) - math.pi / 2.0
+print(alpha)
