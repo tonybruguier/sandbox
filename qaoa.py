aee06088736949d62faf3e940f30aeb8e4c1834f
Tony Bruguier <tony.bruguier@gmail.com>
2021-04-04 06:21:57 +0000

    qaoa

--- qaoa.py
+++ qaoa.py
@@ -15,33 +15,57 @@ def brute_force(graph, n):
     vecs = (-1) ** bitstrings
     vals = 0.5 * np.sum(vecs * (mat @ vecs.T).T, axis=-1)
     vals = 0.5 * (graph.size() - vals)
-    return np.round(vals)
+    return max(np.round(vals))
 
+def qaoa(booleans, repetitions=10, maxiter=50, p=5):
+    name_to_id = hr.get_name_to_id(booleans)
+    hamiltonians = [hr.build_hamiltonian_from_boolean(boolean, name_to_id) for boolean in booleans]
+    qubits = [cirq.NamedQubit(name) for name in name_to_id.keys()]
+
+    def f(x):
+        # Build the circuit.
+        circuit = cirq.Circuit()
+        circuit.append(cirq.H.on_each(*qubits))
+
+        for i in range(p):
+            circuit += hr.build_circuit_from_hamiltonians(hamiltonians, qubits, 2.0 * x[p + i])
+            circuit.append(cirq.rx(2.0 * x[i]).on_each(*qubits))
+
+        circuit.append(cirq.measure(*qubits, key='m'))
+
+        # Measure
+        result = cirq.Simulator().run(circuit, repetitions=repetitions)
+        bitstrings = result.measurements['m']
+
+        # Evaluate
+        values = []
+        for rep in range(repetitions):
+            subs = {name: val == 1 for name, val in zip(name_to_id.keys(), bitstrings[rep, :])}
+            values.append(sum(1 if boolean.subs(subs) else 0 for boolean in booleans))
+
+        return -np.mean(values)
+
+    x0 = np.zeros(2 * p)
+    scipy.optimize.minimize(f, x0, method='COBYLA', options={'maxiter': maxiter, 'disp': True})
+
+    return 0
 
 def main():
     # Set problem parameters
-    n = 6
+    n = 12
+    p = 2
+
     # Generate a random 3-regular graph on n nodes
     graph = networkx.random_regular_graph(3, n)
 
     # Compute best possible cut value via brute force search
-    cuts = brute_force(graph, n)
-    print(cuts)
+    print('Brute force max cut: %d' % (brute_force(graph, n)))
 
     # Build the boolean expressions
     booleans = [parse_expr(f"x{i} ^ x{j}") for i, j in graph.edges]
 
-    name_to_id = hr.get_name_to_id(booleans)
-    hamiltonians = [hr.build_hamiltonian_from_boolean(boolean, name_to_id) for boolean in booleans]
+    print('QAOA max cut: %d' % (qaoa(booleans)))
 
-    qubits = [cirq.NamedQubit(name) for name in name_to_id.keys()]
-    circuit = cirq.Circuit()
-    circuit.append(cirq.H.on_each(*qubits))
-
-    theta = 0.1 * math.pi
-    circuit += hr.build_circuit_from_hamiltonians(hamiltonians, qubits, theta)
-
-    print(circuit)
 
 if __name__ == '__main__':
     main()
