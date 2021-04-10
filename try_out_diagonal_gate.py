429a8d8cfa5ef9de63dfe13df5dc2bbdcdc5d012
Tony Bruguier <tony.bruguier@gmail.com>
2021-04-10 05:45:05 +0000

    diag gate

--- try_out_diagonal_gate.py
+++ try_out_diagonal_gate.py
@@ -24,3 +24,19 @@ hamiltonian = hr.build_hamiltonian_from_boolean(boolean, name_to_id)
 circuit2 += hr.build_circuit_from_hamiltonians([hamiltonian], qubits, theta)
 print(circuit2)
 print(np.diag(cirq.unitary(circuit2)))
+
+
+# q0: ────────────Y^-0.5───@───Y^0.5───Rz(-0.05π)───Y^-0.5───@───Y^0.5───Rz(0)───
+#                          │                                 │
+# q1: ────────────Rz(0)────@─────────────────────────────────@───────────────────
+
+# global phase:
+# [0.99691733+0.0784591j 0.99691733-0.0784591j 0.99691733-0.0784591j
+#  0.99691733+0.0784591j]
+
+
+# q0: ───X───Rz(-0.05π)───X───
+#       │                │
+# q1: ───@────────────────@───
+# [0.99691733+0.0784591j 0.99691733-0.0784591j 0.99691733-0.0784591j
+#  0.99691733+0.0784591j]
