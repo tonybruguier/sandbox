3c83d7b77d4ef5a93dcb47a911e21acce693574a
Tony Bruguier <tony.bruguier@gmail.com>
2021-05-22 13:04:08 +0000

    Add references

---
+++ quantum_embeddings.py
@@ -0,0 +1,7 @@
+# The effect of data encoding on the expressive power of variational quantum machine learning models
+# Maria Schuld, Ryan Sweke, Johannes Jakob Meyer
+# https://arxiv.org/abs/2008.08605
+
+# Quantum embeddings for machine learning
+# Seth Lloyd, Maria Schuld, Aroosa Ijaz, Josh Izaac, Nathan Killoran
+# https://arxiv.org/abs/2001.03622
