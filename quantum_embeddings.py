9090f43dbdf48c0b3ab4e1c15910a610d21ffef0
Tony Bruguier <tony.bruguier@gmail.com>
2021-05-22 13:43:30 +0000

    1D skeleton qembed

--- quantum_embeddings.py
+++ quantum_embeddings.py
@@ -5,3 +5,17 @@
 # Quantum embeddings for machine learning
 # Seth Lloyd, Maria Schuld, Aroosa Ijaz, Josh Izaac, Nathan Killoran
 # https://arxiv.org/abs/2001.03622
+
+import cirq
+
+def build_1d_circuit(x: float, theta: float, depth: int):
+    circuit = cirq.Circuit()
+    q = cirq.NamedQubit('q')
+    for _ in range(depth):
+        circuit.append(cirq.Rx(rads=x).on(q))
+        circuit.append(cirq.Ry(rads=theta).on(q))
+    return circuit
+
+circuit = build_1d_circuit(0.1, 0.2, 3)
+
+print(circuit)
