2d0f366d6262aa2c84864a24972a4828f0b18697
Tony Bruguier <tony.bruguier@gmail.com>
2022-03-20 04:04:21 +0000

    grover

--- grover.py
+++ grover.py
@@ -1,5 +1,5 @@
 import cirq
-# import cirq.contrib.bayesian_network as ccb
+import cirq.contrib.bayesian_network as ccb
 
 def diffuser(qubits):
     for q in qubits:
@@ -13,32 +13,29 @@ def diffuser(qubits):
         yield cirq.H(q)
 
 def oracle(qubits):
-    # gate = BayesianNetworkGate(
-    #     [('q0', 0.5), ('q1', 0.5), ('q2', 0.5), ('q3', None), ('q4', None)],
-    #     [(('q3'), ('q0', 'q1',), [0.01, 0.02, 0.03, 0.04]),
-    #      (('q4'), ('q0', 'q1',), [0.01, 0.02, 0.03, 0.04])])
-
-    # # This Oracle should favor |111>
+    # # This Oracle should favor |11111>
     # yield cirq.Z(qubits[-1]).controlled_by(*qubits[0:-1])
 
-    # This Oracle should favor |??1>
-    yield cirq.Z(qubits[-1])
+    # This Oracle should favor |???1?>
+    yield cirq.Z(qubits[3])
 
-qubits = cirq.LineQubit.range(3)
+
+qubits = cirq.LineQubit.range(5)
 circuit = cirq.Circuit()
 
 for q in qubits:
     circuit += cirq.H(q)
 
+circuit += ccb.BayesianNetworkGate(
+    [('q0', 0.5), ('q1', 0.5), ('q2', 0.5), ('q3', None), ('q4', None)],
+    [(('q3'), ('q0', 'q1',), [0.01, 0.02, 0.03, 0.04]),
+     (('q4'), ('q0', 'q1',), [0.40, 0.10, 0.70, 0.90])]).on(*qubits)
+
 # Contrary to classical algorithms, adding extra iterations will worsen the results.
-for _ in range(2):
+for _ in range(3):
     circuit += oracle(qubits)
     circuit += diffuser(qubits)
-
-print(circuit)
 
 phi = cirq.Simulator().simulate(circuit, qubit_order=qubits, initial_state=0).state_vector()
 
 print(cirq.dirac_notation(phi))
-
-print(cirq.unitary(cirq.Circuit(oracle(qubits))).real)
