1fdd8c98850cbe9c98242eba8e9ac4b80bc75096
Tony Bruguier <tony.bruguier@gmail.com>
2022-03-21 02:48:58 +0000

    grover

--- grover.py
+++ grover.py
@@ -23,19 +23,38 @@ def oracle(qubits):
 qubits = cirq.LineQubit.range(5)
 circuit = cirq.Circuit()
 
-for q in qubits:
-    circuit += cirq.H(q)
+# for q in qubits:
+#     circuit += cirq.H(q)
 
 circuit += ccb.BayesianNetworkGate(
-    [('q0', 0.5), ('q1', 0.5), ('q2', 0.5), ('q3', None), ('q4', None)],
+    [('q0', 0.3), ('q1', 0.6), ('q2', 0.3), ('q3', None), ('q4', None)],
     [(('q3'), ('q0', 'q1',), [0.01, 0.02, 0.03, 0.04]),
-     (('q4'), ('q0', 'q1',), [0.40, 0.10, 0.70, 0.90])]).on(*qubits)
+     (('q4'), ('q2', 'q3',), [0.40, 0.10, 0.70, 0.90])]).on(*qubits)
 
 # Contrary to classical algorithms, adding extra iterations will worsen the results.
-for _ in range(3):
+for _ in range(1):
     circuit += oracle(qubits)
     circuit += diffuser(qubits)
 
 phi = cirq.Simulator().simulate(circuit, qubit_order=qubits, initial_state=0).state_vector()
+probs = abs(phi) ** 2
 
-print(cirq.dirac_notation(phi))
+for shift in range(5):
+    total_prob = 0.0
+    for i, prob in enumerate(probs):
+        if (i >> ( 5 - 1 - shift)) & 0x01:
+            total_prob += prob
+    print(f'p(q{shift} = 1) = {total_prob}')
+
+
+for shift in range(5):
+    prob_num = 0.0
+    prob_den = 0.0
+    for i, prob in enumerate(probs):
+        if (i >> ( 5 - 1 - 3)) & 0x01:
+            prob_den += prob
+            if (i >> ( 5 - 1 - shift)) & 0x01:
+                prob_num += prob
+    print(f'p(q{shift} = 1 | q3 = 1) = {prob_num / prob_den}')
+
+#print(cirq.dirac_notation(phi))
